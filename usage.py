bab1ee908859c8a2775fb02c1bb34fc0ff029eb7
Nick <nikolaiwebguy@gmail.com>
2014-01-21 22:11:07 +1100

    added iinet usage script
    plugin for checking iinet usage from toolbox.
    Only tested against business accounts.

---
+++ usage.py
@@ -0,0 +1,72 @@
+import httplib, urllib, urllib2, re, time
+from HTMLParser import HTMLParser
+
+username = "username"
+password = "password"
+
+curmonth = time.strftime("%Y%m%d")
+
+params = ({"username": username, "password": password, "action": "login","period": curmonth})
+usage_url = "https://toolbox.iinet.net.au/cgi-bin/new/volumeusage.cgi"
+
+opener = urllib2.build_opener()
+req = urllib2.Request(usage_url,urllib.urlencode(params))
+response = opener.open(req)
+
+class MyParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.inUsage = False
+        self.recording = False
+        self.data = []
+
+    def handle_starttag(self, tag, attributes):
+        if self.inUsage:
+            for name, value in attributes:
+                if name == 'class' and value == 'usage_text':
+                    self.recording = True
+                else:
+                    return
+        else:
+            for name, value in attributes:
+                if name == 'id' and value == 'usage_div':
+                    self.inUsage = True
+                else:
+                    return
+
+    def handle_endtag(self, tag):
+        if tag == 'td' and self.inUsage:
+           self.inUsage = False
+        
+        if tag == 'div' and self.recording:
+            self.recording = False
+  
+    def handle_data(self, data):
+        if self.recording and self.inUsage:
+            self.data.append(data)
+
+parser = MyParser()
+parser.feed(response.read())
+
+raw = parser.data
+usage_array = raw[0].replace("MB","").split()
+usage = float(usage_array[0].replace(",",""))
+quota = float(usage_array[3].replace(",",""))
+
+percent_used = int((usage/quota) * 100)
+
+#0 - 75
+if percent_used < 75:
+    print "OK -", str(percent_used)+"% used."
+    exit(0)
+# 75 - 90
+elif percent_used < 90:
+    print "WARNING -",str(percent_used)+"% used."
+    exit(1)
+#90+
+elif percent_used > 90:
+    print "CRITICAL -",str(percent_used)+"% used."
+    exit(2)
+else:
+    print "UNKNOWN - unable to determine use: ",percent_used
+    exit(3)
